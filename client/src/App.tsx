22de3154480b1a0f4551f1116181710765043fa0
TrigamDev <trigambusiness@gmail.com>
2023-09-01 17:23:46 -0400

    Start of client

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './assets/logo.svg';
+import './styles/App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <nav>
+                
+            </nav>
+        </div>
+    );
+}
+
+export default App;
